46e95f4a96c17b3cfb974410f10f0f215b2fda74
Fernando <pupygreen@gmail.com>
2022-07-24 12:27:00 -0300

    Subindo o projeto dashboard

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "Drone",
+    proctNamber: "22",
+    paymentStatus: "22-04-2022",
+    shipping: "Declined",
+  },
+  {
+    productName: "Bola de Futebol",
+    proctNamber: "2",
+    paymentStatus: "21-05-2022",
+    shipping: "Pending",
+  },
+  {
+    productName: "Notebook",
+    proctNamber: "53",
+    paymentStatus: "21-05-2022",
+    shipping: "Pending",
+  },
+  {
+    productName: "Pendrive",
+    proctNamber: "54",
+    paymentStatus: "21-05-2022",
+    shipping: "Declined",
+  },
+  {
+    productName: "Smartphone",
+    proctNamber: "32",
+    paymentStatus: "21-05-2022",
+    shipping: "Pending",
+  },
+  {
+    productName: "Tablet",
+    proctNamber: "65",
+    paymentStatus: "21-05-2022",
+    shipping: "Pending",
+  }
+]
